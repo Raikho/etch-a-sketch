b260b4b6ffc2fd780cd4111661b158eb22bef5c1
Mateen <zahidx297@yahoo.com>
2022-07-16 00:50:05 -0400

    add auto generated 16x grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+const container = document.querySelector('div.container')
+
+const pixels = [];
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('square');
+        // pixel.textContent = `${i},${j}`;
+        row.appendChild(pixel);
+    }
+    pixels[i] = row;
+    container.appendChild(pixels[i])
+}
+
